23790302f53907fb8d0f2d88d1d2ae3df0ee43aa
Chocolatey <chocolatey@realdimensions.net>
2018-01-03 21:46:51 +0000

    AU: 1 updated - dmd.install [skip ci] https://gist.github.com/85027101140f8f5ee3a818155a8e6a63/0f45259dc1bd1250e6d5abb58660b0ae2c29df85

--- automatic/dmd.install/tools/ChocolateyInstall.ps1
+++ automatic/dmd.install/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 # Automatically Updated
-$url = 'http://downloads.dlang.org/releases/2.x/2.077.1/dmd-2.077.1.exe'
-$checksum = '06d877e0f51c2fa839c85230f48b3409d8557c32d20ddfe5254c1c4cf4878bcc'
+$url = 'http://downloads.dlang.org/releases/2.x/2.078.0/dmd-2.078.0.exe'
+$checksum = '73004c52d78c108bb1615d0cffbf94d896e6172216458fa67fc3638e8a226900'
 $checksumType = 'sha256'
 # /Automatically Updated
 
