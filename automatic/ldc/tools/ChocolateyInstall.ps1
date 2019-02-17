e6d93a1ae6dd9fbd74bf7270937f957510af2cf1
Chocolatey <chocolatey@realdimensions.net>
2019-02-17 21:43:50 +0000

    AU: 1 updated - ldc [skip ci] https://gist.github.com/85027101140f8f5ee3a818155a8e6a63/a790fbf40479c1ac4bc1f444ccc16dcffc0e133c

--- automatic/ldc/tools/ChocolateyInstall.ps1
+++ automatic/ldc/tools/ChocolateyInstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   softwareName   = 'ldc*'
 
   # Automatically Updated
-  url            = 'https://github.com/ldc-developers/ldc/releases/download/v1.13.0/ldc2-1.13.0-windows-x86.7z'
-  checksum       = '540387227a0d2701d93af4cf2ff771f2312aba5256404e73453f4cc399def0b6'
+  url            = 'https://github.com/ldc-developers/ldc/releases/download/v1.14.0/ldc2-1.14.0-windows-x86.7z'
+  checksum       = '7f65ccc3dd9a31874efea2852b85332fab6a9bd0959a6ac3b7607b113f2a0be3'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/ldc-developers/ldc/releases/download/v1.13.0/ldc2-1.13.0-windows-x64.7z'
-  checksum64     = 'ce6fb5f0008659b46051641239fb3c573083d70fd24b73225ab3ebe3e309e2ed'
+  url64bit       = 'https://github.com/ldc-developers/ldc/releases/download/v1.14.0/ldc2-1.14.0-windows-x64.7z'
+  checksum64     = '30f35444a6b260206669e16e1af7d31c141efd2f1524fb583483a5d418aa0911'
   checksumType64 = 'sha256'
   # /Automatically Updated
 }
