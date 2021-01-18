a8850cef2245f5bbc65058b87a6b1a306c5bfdd8
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-18 03:58:30 -0600

    Add new names to the remove list

--- automatic/ldc/tools/ChocolateyBeforeModify.ps1
+++ automatic/ldc/tools/ChocolateyBeforeModify.ps1
@@ -6,3 +6,5 @@ $version = '1.24.0'
 
 Remove-Item -Force -Recurse "$(Get-ToolsLocation)/ldc2-$version-win32-msvc"
 Remove-Item -Force -Recurse "$(Get-ToolsLocation)/ldc2-$version-win64-msvc"
+Remove-Item -Force -Recurse "$(Get-ToolsLocation)/ldc2-$version-windows-x86"
+Remove-Item -Force -Recurse "$(Get-ToolsLocation)/ldc2-$version-windows-x64"
